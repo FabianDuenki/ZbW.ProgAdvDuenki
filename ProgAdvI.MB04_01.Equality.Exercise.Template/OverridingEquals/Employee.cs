c8bde03b0bd4e5fc6844d9634e5ec8e4df5885d3
Fabian Duenki <fabian.duenki@buhlergroup.com>
2025-02-25 16:47:54 +0100

    Equality check

--- ProgAdvI.MB04_01.Equality.Exercise.Template/OverridingEquals/Employee.cs
+++ ProgAdvI.MB04_01.Equality.Exercise.Template/OverridingEquals/Employee.cs
@@ -3,5 +3,33 @@
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public bool Equals(Employee? other)
+        {
+            if(ReferenceEquals(null, other)) return false;
+            if(ReferenceEquals(this, other)) return true;
+
+            return string.Equals(FirstName, other.FirstName)
+                && string.Equals(LastName, other.LastName);
+        }
+        public override bool Equals(object? obj)
+        {
+            if(ReferenceEquals (null, obj)) return false;
+
+            return Equals (obj as Employee);
+        }
+        public static bool operator ==(Employee left, Employee right)
+        {
+            if (ReferenceEquals(null, left) || ReferenceEquals(right, null)) return false;
+            return left.Equals(right);
+        }
+        public static bool operator !=(Employee left, Employee right)
+        {
+            return !(left == right);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FirstName, LastName);
+        }
     }
 }
--- ProgAdvI.MB04_01.Equality.Exercise.Template/OverridingEquals/PhoneNumber.cs
+++ ProgAdvI.MB04_01.Equality.Exercise.Template/OverridingEquals/PhoneNumber.cs
@@ -14,5 +14,35 @@ namespace OverridingEquals {
         // TODO: Implement Equals 
         // TODO: Implement == and !=
         // TODO: Implement GetHashCode
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(obj, null)) return false;
+            if (ReferenceEquals(obj, this)) return true;
+            
+            return this.Equals(obj as PhoneNumber);
+        }
+        public bool Equals(PhoneNumber? other) {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return string.Equals(this.AreaCode, other.AreaCode)
+                && string.Equals(this.Exchange, other.Exchange)
+                && string.Equals(this.SubscriberNumber, other.SubscriberNumber);
+        }
+        public static bool operator ==(PhoneNumber? left, PhoneNumber? right)
+        {
+            if(ReferenceEquals(left, right)) return true;
+            if (ReferenceEquals(left, null) || ReferenceEquals(right, null)) return false;
+            return left.Equals(right);
+
+        }
+        public static bool operator !=(PhoneNumber? left, PhoneNumber? right)
+        {
+            return !(left == right);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(AreaCode, Exchange, SubscriberNumber);
+        }   
     }
 }
