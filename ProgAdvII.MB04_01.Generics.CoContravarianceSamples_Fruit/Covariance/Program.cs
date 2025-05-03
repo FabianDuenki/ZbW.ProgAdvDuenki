0eba7dc1e5e34d89a26ead4e091653ae8fd138f6
symas <tk@symas.ch>
2025-05-03 10:27:40 +0200

    rearrange CoContravariance Fruits

--- ProgAdvII.MB04_01.Generics.CoContravarianceSamples_Fruit/Covariance/Program.cs
+++ ProgAdvII.MB04_01.Generics.CoContravarianceSamples_Fruit/Covariance/Program.cs
@@ -1,154 +1,120 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Covariance {
+    internal class Program {
+        public static void Main(string[] args) {
+
+            string[] strings = new string[] { "a", "b", "c" };
+
+            object[] objects = strings;
+
+            objects[1] = 5;
 
 
-using Covariance;
+            FirstSampleWithList();
+            //WithStandardGenerics();
+            //WithHomemadeGeneric();
+            //WithArrays();
+        }
 
 
-Apple[] apples = new Apple[] { new Apple() { Weight = 100 }, new Apple() { Weight = 105 } };
-Fruit[] fruits = apples;
+        static void FirstSampleWithList() {
 
-var weightSum = 0m;
-foreach(var fruit in fruits) {
-    weightSum += fruit.Weight;
+            List<Apple> apples = new List<Apple>();
+            apples.Add(new Apple { Weight = 500 });
+            apples.Add(new Apple { Weight = 550 });
 
-}   
+            var totalApples = SumWeights(apples); // Was könnte der Grund sein, dass das nicht geht?
 
+            List<Banana> bananas = new List<Banana>();
+            bananas.Add(new Banana { Weight = 800 });
+            bananas.Add(new Banana { Weight = 790 });
 
-fruits[1] = new Banana();
+            var totalBananas = SumWeights(bananas);
+        }
 
+        static void FirstSample() {
+            BagOfFruits fruits = new BagOfApples();
+            fruits.Add(new Apple());
+            fruits.Add(new Banana());
 
+            Console.WriteLine(fruits.Get(0));
+            Console.WriteLine(fruits.Get(1));
 
-string[] strings = new string[] { "a", "b", "c" };
+            BagOfApples apples = (BagOfApples)fruits;
+            Console.WriteLine(apples.Get(0));
+            Console.WriteLine(apples.Get(1));
+        }
 
-object[] objects = strings;
+        static decimal SumWeights(List<Fruit> fruits) {
+            var ret = 0m;
+            foreach (var fruit in fruits) {
+                ret += fruit.Weight;
+            }
 
-objects[1] = 5;
+            //fruits.Add(new Banana() {Weight = 450});
 
+            return ret;
+        }
 
+        static void WithStandardGenerics() {
+            var bagOfApples = new List<Apple>() {
+                new(), new()
+            };
 
+            IEnumerable<Fruit> bagOfFruit;
 
-FirstSampleWithList();
-//FirstSample();
-//WithStandardGenerics();
-//WithHomemadeGeneric();
-//WithArrays();
+            bagOfFruit = bagOfApples;
 
+            foreach (Fruit fruit in bagOfFruit)
+                Console.WriteLine(fruit);
 
+            bagOfApples.Add(new Apple());
 
-void FirstSampleWithList() {
+            Console.WriteLine("---");
 
-    List<Apple> apples = new List<Apple>();
-    apples.Add(new Apple {Weight = 500});
-    apples.Add(new Apple {Weight = 550});
+            foreach (Fruit fruit in bagOfFruit)
+                Console.WriteLine(fruit);
 
-    IEnumerable<Apple> covariantApples = apples;
+            //bagOfApples.Add(new Banana());
+            //bagOfFruit.Add(new Banana ());
 
-   
-    var totalApples = SumWeights(covariantApples); // Was könnte der Grund sein, dass das nicht geht?
+            Console.WriteLine("---");
 
-    List<Banana> bananas = new List<Banana>();
-    bananas.Add(new Banana { Weight = 800 });
-    bananas.Add(new Banana { Weight = 790 });
+            foreach (Fruit fruit in bagOfFruit)
+                Console.WriteLine(fruit);
+        }
 
-    var totalBananas = SumWeights(bananas);
-}
+        static void WithHomemadeGeneric() {
+            var bagOfApples = new Bag<Apple>();
 
-void FirstSample() {
-    BagOfFruits fruits = new BagOfApples();
-    fruits.Add(new Apple());
-    fruits.Add(new Banana());
+            bagOfApples.Add(new Apple());
+            bagOfApples.Add(new Apple());
+            bagOfApples.Add(new Apple());
 
-    Console.WriteLine(fruits.Get(0));
-    Console.WriteLine(fruits.Get(1));
+            IBag<Fruit> bagOfFruit = bagOfApples;
 
-    BagOfApples apples = (BagOfApples)fruits;
-    Console.WriteLine(apples.Get(0));
-    Console.WriteLine(apples.Get(1));
-}
+            Console.WriteLine(bagOfFruit.Get(0));
+            Console.WriteLine(bagOfFruit.Get(1));
 
-decimal SumWeights(IEnumerable<Fruit> fruits) {
-    var ret = 0m;
-    foreach (var fruit in fruits) {
-        ret += fruit.Weight;
+            //bagOfApples.Add(new Banana());
+            //bagOfFruit.Add(new Banana());
+        }
+
+        static void WithArrays() {
+            var arrayOfApples = new Apple[] {
+                new Apple(),
+                new Apple(),
+                new Apple()
+            };
+
+            Fruit[] arrayOfFruit = arrayOfApples;
+
+            foreach (Fruit fruit in arrayOfFruit)
+                Console.WriteLine(fruit);
+
+            arrayOfFruit[1] = new Banana();
+
+            foreach (Apple apple in arrayOfApples)
+                Console.WriteLine(apple);
+        }
     }
-
-//    fruits.Add(new Banana());
-
-    return ret;
-}
-
-
-
-
-void WithStandardGenerics() {
-    var bagOfApples = new List<Apple>() {
-        new(), new()
-    };
-
-    IEnumerable<Fruit> bagOfFruit;
-
-    bagOfFruit = bagOfApples;
-
-    foreach (Fruit fruit in bagOfFruit)
-        Console.WriteLine(fruit);
-
-    bagOfApples.Add(new Apple());
-
-    Console.WriteLine("---");
-
-    foreach (Fruit fruit in bagOfFruit)
-        Console.WriteLine(fruit);
-
-    //bagOfApples.Add(new Banana());
-    //bagOfFruit.Add(new Banana ());
-
-    Console.WriteLine("---");
-
-    foreach (Fruit fruit in bagOfFruit)
-        Console.WriteLine(fruit);
-}
-
-void WithHomemadeGeneric() {
-    var bagOfApples = new Bag<Apple>();
-
-    bagOfApples.Add(new Apple());
-    bagOfApples.Add(new Apple());
-    bagOfApples.Add(new Apple());
-
-    IBag<Fruit> bagOfFruit = bagOfApples;
-
-    Console.WriteLine(bagOfFruit.Get(0));
-    Console.WriteLine(bagOfFruit.Get(1));
-
-    //bagOfApples.Add(new Banana());
-    //bagOfFruit.Add(new Banana());
-}
-
-void WithArrays() {
-    var arrayOfApples = new Apple[] {
-        new Apple(),
-        new Apple(),
-        new Apple()
-    };
-
-    Fruit[] arrayOfFruit = arrayOfApples;
-
-    foreach (Fruit fruit in arrayOfFruit)
-        Console.WriteLine(fruit);
-
-    arrayOfFruit[1] = new Banana();
-
-    foreach (Apple apple in arrayOfApples)
-        Console.WriteLine(apple);
-}
-
-
-public interface ITest<out T> {
-    //void Add(T item);
-
-    T Get(int index);
-
-    T Foo(int test);
-
-    //T Foo2(T test);
 }
