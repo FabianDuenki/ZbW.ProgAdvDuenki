a4c8fecab90fb650d4709e831657b1010762b2ea
Fabian Duenki <fabian.duenki@buhlergroup.com>
2025-05-27 19:35:16 +0200

    LINQ 1

--- ProgAdvII.MB08_02.Linq.Excercises/Vorgabe/1.1_ExtensionMethods_QueryOperators/Extensions.cs
+++ ProgAdvII.MB08_02.Linq.Excercises/Vorgabe/1.1_ExtensionMethods_QueryOperators/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Markup;
 
 namespace _1._1_ExtensionMethods_QueryOperators {
     /// <summary>
@@ -11,10 +13,39 @@ namespace _1._1_ExtensionMethods_QueryOperators {
     /// Sie finden dort auch die Beschreibung, was die Methode effektiv für einen Effekt hat.
     /// </summary>
     public static class Extensions {
-        // TODO: ZbwForEach
-        // TODO: ZbwWhere
-        // TODO: ZbwOfType
-        // TODO: ZbwToList
-        // TODO: ZbwSum
+        public static void ZbwForEach<T>(this IEnumerable<T> values, Action<T> action)
+        {
+            foreach (var item in values)
+            {
+                action(item);
+            }
+        }
+        public static IEnumerable<T> ZbwWhere<T>(this IEnumerable<T> values, Predicate<T> predicate)
+        {
+            foreach (var item in values)
+            {
+                if (predicate(item)) yield return item;
+            }
+        }
+        public static IEnumerable<T> ZbwOfType<T>(this IEnumerable values)
+        {
+            foreach(var item in values)
+            {
+                if (item is T itemT) yield return itemT;
+            }
+        }
+        public static List<T> ZbwToList<T>(this IEnumerable<T> values)
+        {
+            return values.ToList();
+        }
+        public static int ZbwSum<T>(this IEnumerable<T> values, Func<T, int> selector)
+        {
+            int sum = 0;
+            foreach (var item in values)
+            {
+                sum += selector(item);
+            }
+            return sum;
+        }
     }
 }
--- ProgAdvII.MB08_02.Linq.Excercises/Vorgabe/1.2_ExtensionMethods_Exception/Extensions.cs
+++ ProgAdvII.MB08_02.Linq.Excercises/Vorgabe/1.2_ExtensionMethods_Exception/Extensions.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _1._2_ExtensionMethods_Exceptions {
-    public static class Extensions {
-        // TODO: GetInnerstException
-        // TODO: ForEachException
+    public static class Extensions
+    {
+        public static Exception GetInnerstException(this Exception exception)
+        {
+            return exception.InnerException?.GetInnerstException() ?? exception.InnerException;
+        }
+        public static void ForEachException(this Exception exception, Action<Exception> action)
+        {
+            action(exception);
+            exception.InnerException?.ForEachException(action);
+        }
     }
 }
