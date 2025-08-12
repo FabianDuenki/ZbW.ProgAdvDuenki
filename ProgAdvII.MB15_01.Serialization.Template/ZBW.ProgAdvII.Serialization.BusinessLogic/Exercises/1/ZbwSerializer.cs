7f1cd463f4cd40805286d7c005b4abc7e403140e
Fabian Duenki <fabian.duenki@buhlergroup.com>
2025-08-12 18:32:12 +0200

    PGA2 MB16:Serialization 2

--- ProgAdvII.MB15_01.Serialization.Template/ZBW.ProgAdvII.Serialization.BusinessLogic/Exercises/1/ZbwSerializer.cs
+++ ProgAdvII.MB15_01.Serialization.Template/ZBW.ProgAdvII.Serialization.BusinessLogic/Exercises/1/ZbwSerializer.cs
@@ -1,14 +1,30 @@
-﻿using System;
+﻿using PeterO.Cbor;
+using System;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace ZBW.ProgAdvII.Serialization.BusinessLogic.Exercises._1 {
     public class ZbwSerializer : ISerializer {
         public void Serialize(Student student, Stream stream) {
-            throw new NotImplementedException();
+            var cbor = CBORObject.NewMap()
+                .Add("FirstName", student.FirstName)
+                .Add("LastName", student.LastName)
+                .Add("DateOfBirth", student.DateOfBirth.ToLocalTime());
+            byte[] buffer = cbor.EncodeToBytes();
+
+            var fileStream = File.Open("student.dat", FileMode.Create);
+            fileStream.Write(buffer);
+            fileStream.Close();
         }
 
-        public Student Deserialize(Stream stream) {
-            throw new NotImplementedException();
+        public Student Deserialize(Stream stream)
+        {
+            var cbor = CBORObject.DecodeFromBytes(
+                File.ReadAllBytes("student.dat"), CBOREncodeOptions.Default);
+
+            var student = new Student();
+            return student;
         }
     }
 }
